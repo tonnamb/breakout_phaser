554afaaeca57e304d77475f42ff1631c2376208a
Tonnam Balankura <tonnam.balankura@gmail.com>
2016-08-14 10:19:23 -0400

    brick collision

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ function create() {
 
 function update() {
     game.physics.arcade.collide(ball, paddle);
+    game.physics.arcade.collide(ball, bricks, ballHitBrick);
     paddle.x = game.input.x || game.world.width*0.5;
 }
 
@@ -81,4 +82,8 @@ function initBricks() {
             bricks.add(newBrick);
         }
     }
+}
+
+function ballHitBrick(ball, brick) {
+    brick.kill();
 }
