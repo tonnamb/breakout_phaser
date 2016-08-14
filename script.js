18b605d347c21252b9fbfab8c6b543ef5cb1d3ec
Tonnam Balankura <tonnam.balankura@gmail.com>
2016-08-14 09:24:03 -0400

    background color

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
+
+    game.stage.backgroundColor = "#eee";
 }
 
 function create() {
